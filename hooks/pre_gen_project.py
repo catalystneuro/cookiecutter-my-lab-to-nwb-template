634c06678a64d141aa2a9ddc79b1b8efdf1a7270
h-mayorquin <h.mayorquin@gmail.com>
2022-06-18 18:17:59 -0600

    added cammel case to context

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,3 +1,5 @@
 """
 {{cookiecutter.update({'project_slug': cookiecutter.project.lower().replace(' ', '_').replace('-', '_')})}}
+{{cookiecutter.update({'conversion_name_camel_case': cookiecutter.conversion_name.replace('_', ' ').title().replace(' ', '')})}}
+
 """
